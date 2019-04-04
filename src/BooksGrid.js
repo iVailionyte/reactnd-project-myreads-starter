50410726cf01126d336756b9439505284b9dea2d
Indre <indre.vailionyte@moovel.com>
2019-04-04 09:55:41 +0200

    feat: update Books component

--- src/BooksGrid.js
+++ src/BooksGrid.js
@@ -5,7 +5,7 @@ const BooksGrid = props => (
   <ol className="books-grid">
     {props.books.map((book) => (
       <li key={book.id}>
-        <Book />
+        <Book book={book}/>
       </li>
     ))}
   </ol>
